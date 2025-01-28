8128c4b8d983383921c0ae9deff833834e828b70
HamzaWebPro <hamza.mern2201@gmail.com>
2025-01-28 08:58:21 +0600

    added

--- helpers/emailValidation.js
+++ helpers/emailValidation.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-
+// hamza
 exports.emailValidation = (email) => {
   return String(email).toLowerCase().match(
     /^(?=[a-zA-Z0-9@]{1,256})(?:(?:[a-zA-Z0-9][a-zA-Z0-9._%+-]{0,63})|(?:\"[^\"]{0,62}\"))@(?:(?:[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9])|(?:\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\]))\.(?:[a-zA-Z]{2,63})$/
